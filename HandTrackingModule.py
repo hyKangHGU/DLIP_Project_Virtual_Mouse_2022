135e2f40ad41c761bd60627767b3c13d36ba7bfb
hyKang <91526930+hyKangHGU@users.noreply.github.com>
2022-06-14 10:47:15 +0900

    Add files via upload

---
+++ HandTrackingModule.py
@@ -0,0 +1,120 @@
+import cv2 as cv
+import mediapipe as mp
+import math
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=2,modelC=1, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelC = modelC
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelC, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+        xList = []
+        yList = []
+        bbox = []
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+                # print(id, cx, cy)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv.circle(img, (cx, cy), 5, (255, 0, 255), cv.FILLED)
+
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
+                              (0, 255, 0), 2)
+
+        return self.lmList, bbox
+
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # Fingers
+        for id in range(1, 5):
+
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        # totalFingers = fingers.count(1)
+
+        return fingers
+
+    def drawCircleUpFingers(self, p1, p2, img, draw=True,r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv.circle(img, (x1, y1), r, (255, 0, 255), cv.FILLED)
+            cv.circle(img, (x2, y2), r, (255, 0, 255), cv.FILLED)
+
+        return img, [cx, cy]
+
+    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv.circle(img, (x1, y1), r, (255, 0, 255), cv.FILLED)
+            cv.circle(img, (x2, y2), r, (255, 0, 255), cv.FILLED)
+            cv.circle(img, (cx, cy), r, (0, 0, 255), cv.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+    
+    def tipsPosition(self, pt):
+        x, y = self.lmList[pt][1:]
+        return x, y
+
+    @staticmethod
+    def get_max(value1, value2):
+        '''compares two value to get max value'''
+        if(value1 > value2): maxvalue = value1
+        else               : maxvalue = value2
+        return maxvalue
+    
+    @staticmethod
+    def get_min(value1, value2):
+        '''compares two value to get min value'''
+        if(value1 < value2): minvalue = value1
+        else               : minvalue = value2
+        return minvalue
